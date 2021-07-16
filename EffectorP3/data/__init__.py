e38d603ff0bb18e9ed5be94a49266f86e7bb5334
Darcy Jones <darcy.ab.jonoes@gmail.com>
2021-07-16 14:30:11 +0800

    Replaced hard-coded file paths with dynamic ones.

---
+++ EffectorP3/data/__init__.py
@@ -0,0 +1,112 @@
+from importlib.resources import path as resource_filename
+
+WEKA_PATH = resource_filename(__name__ + ".weka384", 'weka.jar')
+FASTA_PATH = resource_filename(__name__, "Effectors.fasta")
+
+models_bayes_cytoplasmic_fungionly = [
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Mycorrhizal_Bayes', 'trainingdata_iteration85_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Mycorrhizal_Bayes', 'trainingdata_iteration75_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Mycorrhizal_Bayes', 'trainingdata_iteration100_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Mycorrhizal_Bayes', 'trainingdata_iteration41_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Mycorrhizal_Bayes', 'trainingdata_iteration31_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Pathogens_Bayes', 'trainingdata_iteration92_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Pathogens_Bayes', 'trainingdata_iteration16_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Pathogens_Bayes', 'trainingdata_iteration84_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Pathogens_Bayes', 'trainingdata_iteration2_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Pathogens_Bayes', 'trainingdata_iteration96_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Saprophytes_Bayes', 'trainingdata_iteration34_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Saprophytes_Bayes', 'trainingdata_iteration64_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Saprophytes_Bayes', 'trainingdata_iteration88_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Saprophytes_Bayes', 'trainingdata_iteration23_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Saprophytes_Bayes', 'trainingdata_iteration32_ratio3.model')
+]
+
+models_J48_cytoplasmic_fungionly = [
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Mycorrhizal_J48', 'trainingdata_iteration77_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Mycorrhizal_J48', 'trainingdata_iteration61_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Mycorrhizal_J48', 'trainingdata_iteration95_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Mycorrhizal_J48', 'trainingdata_iteration46_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Mycorrhizal_J48', 'trainingdata_iteration56_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Pathogens_J48', 'trainingdata_iteration85_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Pathogens_J48', 'trainingdata_iteration58_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Pathogens_J48', 'trainingdata_iteration87_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Pathogens_J48', 'trainingdata_iteration25_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Pathogens_J48', 'trainingdata_iteration97_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Saprophytes_J48', 'trainingdata_iteration85_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Saprophytes_J48', 'trainingdata_iteration13_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Saprophytes_J48', 'trainingdata_iteration22_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Saprophytes_J48', 'trainingdata_iteration91_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_CytoplasmicFungiOnly_Saprophytes_J48', 'trainingdata_iteration21_ratio3.model')
+]
+
+models_bayes_cytoplasmic = [
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Mycorrhizal_Bayes', 'trainingdata_iteration39_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Mycorrhizal_Bayes', 'trainingdata_iteration17_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Mycorrhizal_Bayes', 'trainingdata_iteration76_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Mycorrhizal_Bayes', 'trainingdata_iteration74_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Mycorrhizal_Bayes', 'trainingdata_iteration78_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Pathogens_Bayes', 'trainingdata_iteration79_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Pathogens_Bayes', 'trainingdata_iteration1_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Pathogens_Bayes', 'trainingdata_iteration32_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Pathogens_Bayes', 'trainingdata_iteration70_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Pathogens_Bayes', 'trainingdata_iteration77_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Saprophytes_Bayes', 'trainingdata_iteration14_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Saprophytes_Bayes', 'trainingdata_iteration71_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Saprophytes_Bayes', 'trainingdata_iteration44_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Saprophytes_Bayes', 'trainingdata_iteration62_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Saprophytes_Bayes', 'trainingdata_iteration64_ratio3.model')
+]
+
+models_J48_cytoplasmic = [
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Mycorrhizal_J48', 'trainingdata_iteration36_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Mycorrhizal_J48', 'trainingdata_iteration14_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Mycorrhizal_J48', 'trainingdata_iteration63_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Mycorrhizal_J48', 'trainingdata_iteration60_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Mycorrhizal_J48', 'trainingdata_iteration29_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Pathogens_J48', 'trainingdata_iteration67_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Pathogens_J48', 'trainingdata_iteration92_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Pathogens_J48', 'trainingdata_iteration78_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Pathogens_J48', 'trainingdata_iteration1_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Pathogens_J48', 'trainingdata_iteration34_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Saprophytes_J48', 'trainingdata_iteration36_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Saprophytes_J48', 'trainingdata_iteration71_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Saprophytes_J48', 'trainingdata_iteration67_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Saprophytes_J48', 'trainingdata_iteration91_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Cytoplasmic_Saprophytes_J48', 'trainingdata_iteration70_ratio3.model')
+]
+
+models_bayes_apoplastic = [
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Animal_Bayes', 'trainingdata_iteration19_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Animal_Bayes', 'trainingdata_iteration70_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Animal_Bayes', 'trainingdata_iteration78_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Animal_Bayes', 'trainingdata_iteration80_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Animal_Bayes', 'trainingdata_iteration92_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Pathogens_Bayes', 'trainingdata_iteration40_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Pathogens_Bayes', 'trainingdata_iteration31_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Pathogens_Bayes', 'trainingdata_iteration98_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Pathogens_Bayes', 'trainingdata_iteration76_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Pathogens_Bayes', 'trainingdata_iteration53_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Saprophytes_Bayes', 'trainingdata_iteration61_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Saprophytes_Bayes', 'trainingdata_iteration79_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Saprophytes_Bayes', 'trainingdata_iteration29_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Saprophytes_Bayes', 'trainingdata_iteration91_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Saprophytes_Bayes', 'trainingdata_iteration32_ratio3.model')
+]
+
+models_J48_apoplastic = [
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Animal_J48', 'trainingdata_iteration82_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Animal_J48', 'trainingdata_iteration19_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Animal_J48', 'trainingdata_iteration46_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Animal_J48', 'trainingdata_iteration47_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Animal_J48', 'trainingdata_iteration100_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Pathogens_J48', 'trainingdata_iteration26_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Pathogens_J48', 'trainingdata_iteration98_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Pathogens_J48', 'trainingdata_iteration49_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Pathogens_J48', 'trainingdata_iteration9_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Pathogens_J48', 'trainingdata_iteration78_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Saprophytes_J48', 'trainingdata_iteration25_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Saprophytes_J48', 'trainingdata_iteration95_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Saprophytes_J48', 'trainingdata_iteration46_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Saprophytes_J48', 'trainingdata_iteration92_ratio3.model'),
+    resource_filename(__name__ + '.TrainingData_Apoplastic_Saprophytes_J48', 'trainingdata_iteration77_ratio3.model')
+]
--- EffectorP3/functions.py
+++ EffectorP3/functions.py
@@ -16,11 +16,11 @@
 """
 # -----------------------------------------------------------------------------------------------------------
 import os
+from os.path import join as pjoin
 import sys
 import subprocess
 import io
 import getopt
-from pkg_resources import resource_filename
 # -----------------------------------------------------------------------------------------------------------
 # Global variables
 # -----------------------------------------------------------------------------------------------------------
@@ -63,106 +63,7 @@ ARFF_HEADER = '''@RELATION effectors
 @DATA
 '''
 # -----------------------------------------------------------------------------------------------------------
-#SCRIPT_PATH = sys.path[0]
-# Empty string is just because resource filename expects to add the filename
-# itself, but we're adding the path separately
-SCRIPT_PATH = resource_filename("EffectorP3.data", "")
 
-models_bayes_cytoplasmic_fungionly = [SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Mycorrhizal_Bayes//trainingdata_iteration85_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Mycorrhizal_Bayes//trainingdata_iteration75_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Mycorrhizal_Bayes//trainingdata_iteration100_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Mycorrhizal_Bayes//trainingdata_iteration41_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Mycorrhizal_Bayes//trainingdata_iteration31_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Pathogens_Bayes//trainingdata_iteration92_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Pathogens_Bayes//trainingdata_iteration16_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Pathogens_Bayes//trainingdata_iteration84_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Pathogens_Bayes//trainingdata_iteration2_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Pathogens_Bayes//trainingdata_iteration96_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Saprophytes_Bayes//trainingdata_iteration34_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Saprophytes_Bayes//trainingdata_iteration64_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Saprophytes_Bayes//trainingdata_iteration88_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Saprophytes_Bayes//trainingdata_iteration23_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Saprophytes_Bayes//trainingdata_iteration32_ratio3.model']
-
-models_J48_cytoplasmic_fungionly = [SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Mycorrhizal_J48//trainingdata_iteration77_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Mycorrhizal_J48//trainingdata_iteration61_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Mycorrhizal_J48//trainingdata_iteration95_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Mycorrhizal_J48//trainingdata_iteration46_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Mycorrhizal_J48//trainingdata_iteration56_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Pathogens_J48//trainingdata_iteration85_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Pathogens_J48//trainingdata_iteration58_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Pathogens_J48//trainingdata_iteration87_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Pathogens_J48//trainingdata_iteration25_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Pathogens_J48//trainingdata_iteration97_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Saprophytes_J48//trainingdata_iteration85_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Saprophytes_J48//trainingdata_iteration13_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Saprophytes_J48//trainingdata_iteration22_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Saprophytes_J48//trainingdata_iteration91_ratio3.model',
-SCRIPT_PATH + '/TrainingData_CytoplasmicFungiOnly_Saprophytes_J48//trainingdata_iteration21_ratio3.model']
-
-models_bayes_cytoplasmic = [SCRIPT_PATH + '/TrainingData_Cytoplasmic_Mycorrhizal_Bayes//trainingdata_iteration39_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Mycorrhizal_Bayes//trainingdata_iteration17_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Mycorrhizal_Bayes//trainingdata_iteration76_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Mycorrhizal_Bayes//trainingdata_iteration74_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Mycorrhizal_Bayes//trainingdata_iteration78_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Pathogens_Bayes//trainingdata_iteration79_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Pathogens_Bayes//trainingdata_iteration1_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Pathogens_Bayes//trainingdata_iteration32_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Pathogens_Bayes//trainingdata_iteration70_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Pathogens_Bayes//trainingdata_iteration77_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Saprophytes_Bayes//trainingdata_iteration14_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Saprophytes_Bayes//trainingdata_iteration71_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Saprophytes_Bayes//trainingdata_iteration44_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Saprophytes_Bayes//trainingdata_iteration62_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Saprophytes_Bayes//trainingdata_iteration64_ratio3.model']
-
-models_J48_cytoplasmic = [SCRIPT_PATH + '/TrainingData_Cytoplasmic_Mycorrhizal_J48//trainingdata_iteration36_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Mycorrhizal_J48//trainingdata_iteration14_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Mycorrhizal_J48//trainingdata_iteration63_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Mycorrhizal_J48//trainingdata_iteration60_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Mycorrhizal_J48//trainingdata_iteration29_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Pathogens_J48//trainingdata_iteration67_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Pathogens_J48//trainingdata_iteration92_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Pathogens_J48//trainingdata_iteration78_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Pathogens_J48//trainingdata_iteration1_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Pathogens_J48//trainingdata_iteration34_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Saprophytes_J48//trainingdata_iteration36_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Saprophytes_J48//trainingdata_iteration71_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Saprophytes_J48//trainingdata_iteration67_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Saprophytes_J48//trainingdata_iteration91_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Cytoplasmic_Saprophytes_J48//trainingdata_iteration70_ratio3.model']
-
-models_bayes_apoplastic = [SCRIPT_PATH + '/TrainingData_Apoplastic_Animal_Bayes//trainingdata_iteration19_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Animal_Bayes//trainingdata_iteration70_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Animal_Bayes//trainingdata_iteration78_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Animal_Bayes//trainingdata_iteration80_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Animal_Bayes//trainingdata_iteration92_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Pathogens_Bayes//trainingdata_iteration40_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Pathogens_Bayes//trainingdata_iteration31_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Pathogens_Bayes//trainingdata_iteration98_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Pathogens_Bayes//trainingdata_iteration76_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Pathogens_Bayes//trainingdata_iteration53_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Saprophytes_Bayes//trainingdata_iteration61_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Saprophytes_Bayes//trainingdata_iteration79_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Saprophytes_Bayes//trainingdata_iteration29_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Saprophytes_Bayes//trainingdata_iteration91_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Saprophytes_Bayes//trainingdata_iteration32_ratio3.model']
-
-models_J48_apoplastic = [SCRIPT_PATH + '/TrainingData_Apoplastic_Animal_J48//trainingdata_iteration82_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Animal_J48//trainingdata_iteration19_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Animal_J48//trainingdata_iteration46_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Animal_J48//trainingdata_iteration47_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Animal_J48//trainingdata_iteration100_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Pathogens_J48//trainingdata_iteration26_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Pathogens_J48//trainingdata_iteration98_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Pathogens_J48//trainingdata_iteration49_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Pathogens_J48//trainingdata_iteration9_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Pathogens_J48//trainingdata_iteration78_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Saprophytes_J48//trainingdata_iteration25_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Saprophytes_J48//trainingdata_iteration95_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Saprophytes_J48//trainingdata_iteration46_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Saprophytes_J48//trainingdata_iteration92_ratio3.model',
-SCRIPT_PATH + '/TrainingData_Apoplastic_Saprophytes_J48//trainingdata_iteration77_ratio3.model']
 # -----------------------------------------------------------------------------------------------------------
 # Hydrophobicity (Fauchere and Pliska, 1983)
 HYDRO_DIC = {
@@ -537,6 +438,12 @@ def SimpleFastaParser(handle):
     yield title, "".join(lines).replace(" ", "").replace("\r", "")
 # -----------------------------------------------------------------------------------------------------------
 def get_effector_predictions(ORIGINAL_IDENTIFIERS, SEQUENCES, EFFECTOR_THRESHOLD, ensembl_votes_cytoplasmic, ensembl_votes_apoplastic):
+    from .data import (
+        models_bayes_cytoplasmic,
+        models_J48_cytoplasmic,
+        models_bayes_apoplastic,
+        models_J48_apoplastic
+    )
 
     models_cytoplasmic = models_bayes_cytoplasmic + models_J48_cytoplasmic
     models_apoplastic = models_bayes_apoplastic + models_J48_apoplastic
@@ -624,9 +531,9 @@ def get_model_predictions(WEKA_PATH, RESULTS_PATH, MODELS, CLASSIFIER, ensembl_v
 
     for model in MODELS:
         #--------------------------------------------------------------
-        ParamList = ['java', '-cp', WEKA_PATH, CLASSIFIER, '-l', model, '-T', RESULTS_PATH + 'weka.arff', '-p', 'first-last']
+        ParamList = ['java', '-cp', WEKA_PATH, CLASSIFIER, '-l', model, '-T', pjoin(RESULTS_PATH, 'weka.arff'), '-p', 'first-last']
 
-        with open(RESULTS_PATH + 'Predictions.txt', 'wb') as out:
+        with open(pjoin(RESULTS_PATH, 'Predictions.txt'), 'wb') as out:
             try:
                 Process = subprocess.Popen(ParamList, shell=False, stdout=out)
                 sts = Process.wait()
@@ -644,7 +551,7 @@ def get_model_predictions(WEKA_PATH, RESULTS_PATH, MODELS, CLASSIFIER, ensembl_v
                 sys.exit(1)
         #--------------------------------------------------------------
         # Parse the WEKA output file
-        file_input = RESULTS_PATH + 'Predictions.txt'
+        file_input = pjoin(RESULTS_PATH, 'Predictions.txt')
         predicted_effectors, predicted_noneffectors, predictions = parse_weka_output(file_input, ORIGINAL_IDENTIFIERS, SEQUENCES)
 
         for index, (ident, prediction, prob, seq) in enumerate(predictions):
